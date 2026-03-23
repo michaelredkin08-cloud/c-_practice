7cf525c7045ab3162d7db8e1a3346d886e63a274
michaelredkin08-cloud <michaelredkin08@gmail.com>
2026-03-23 12:37:32 +0000

    Enhance match handling in football program
    Added a variable for number of matches and implemented logic for handling automatic wins and results input.

--- year1/semester2/week10/ObjectOrientedFootball/Program.cs
+++ year1/semester2/week10/ObjectOrientedFootball/Program.cs
@@ -7,7 +7,8 @@ namespace ObjectOrientedChallengeFootball
         static void Main(string[] args)
         {
             //Declare
-            int numOfTeams = 5; // Number o teams if odd there will be bye-in;
+            int numOfTeams = 5, numOfMatches = 0; // Number of teams if odd there will be bye-in;
+
             List<Team> team = new List<Team>();
             for (int i = 0; i < numOfTeams; i++)//Entering the name for all the teams
             {
@@ -18,9 +19,23 @@ namespace ObjectOrientedChallengeFootball
             }
             do
             {
-                
-            }while();
-            
+                for (int i = numOfTeams; i > 0; i--)
+                {
+                    if (i == 1 && numOfTeams % 2 != 0)
+                    {
+                        team[i].Result = "Automatic Win";
+                        team[i].Round = numOfMatches + 1;
+                        team[i].ByeIn = "Yes";
+                    }
+                    WriteLine($"Enter the Result for the team {team[i].Name} , is it win or loss , leave byeIn to the last team");
+                    team[i].Result = ReadLine();
+                    team[i].Round = numOfMatches + 1;
+                    numOfMatches++;
+                }
+
+
+            } while ();
+
         }
     }
 }
