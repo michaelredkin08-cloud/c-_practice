201e45e17bde5e627647b92f660f5676b54d145b
michaelredkin08-cloud <michaelredkin08@gmail.com>
2026-03-23 11:34:39 +0000

    Create Program.cs for football team management
    Initialize program to manage football teams with user input for names and locations.

---
+++ year1/semester2/week10/ObjectOrientedFootball/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using static System.Console;
+namespace ObjectOrientedChallengeFootball
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare
+            int numOfTeams = 5; // Number o teams if odd there will be bye-in;
+            List<Team> team = new List<Team>();
+            for (int i = 0; i < numOfTeams; i++)//Entering the name for all the teams
+            {
+                WriteLine("Enter the team Name ");
+                team[i].Name = ReadLine();
+                WriteLine("Is it home or away team");
+                team[i].Place = ReadLine();
+            }
+            
+        }
+    }
+}
