14875b49f3b1f4480b1ae6d209bd227e78b2a168
michaelredkin08-cloud <michaelredkin08@gmail.com>
2026-03-23 11:35:27 +0000

    Add files via upload

---
+++ year1/semester2/week10/ObjectOrientedFootball/History.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ObjectOrientedChallengeFootball
+{
+    internal class History
+    {
+        public List<History> Rounds = new List<History>(); 
+
+        public string Round { get; set; }
+        public History(Team team, string result , string round , string byeIn)
+        {
+            Round = round;
+        }
+    }
+}
---
+++ year1/semester2/week10/ObjectOrientedFootball/Team.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedChallengeFootball
+{
+    internal class Team
+    {
+        public string Name { get; set; }
+
+        public string Place { get; set; }
+
+        public Team(string name, string place)
+        {
+            Name = name;
+            Place = place;
+        }
+    }
+}
